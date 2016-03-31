9dd117241a11055efe967bd435e49e3717bde8a7
Tony Won <tonywon89@gmail.com>
2016-03-31 11:32:45 -0700

    refactor code for logging in and logging out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,20 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find_by(session_token: session[:session_token])
   end
 
-  helper_method :current_user
+  def login!(user)
+    user.reset_session_token!
+    session[:session_token] = user.session_token
+  end
+
+  def logout!
+    current_user.reset_session_token!
+    @current_user = nil
+    session[:session_token] = nil
+  end
+
+  def logged_in?
+    current_user ? true : false
+  end
+
+  helper_method :current_user, :logged_in?
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,20 +11,18 @@ class SessionsController < ApplicationController
     @user = User.find_by_credentials(email, password)
 
     if @user
-      @user.reset_session_token!
-      session[:session_token] = @user.session_token
+      login!(@user)
       redirect_to users_url
     else
-      @user = User.new(emali: email)
+      flash.now[:errors] = "Invalid credentials"
+      @user = User.new(email: email)
       render :new
     end
   end
 
   def destroy
-    if current_user
-      current_user.reset_session_token!
-      @current_user = nil
-      session[:session_token] = nil
+    if logged_in?
+      logout!
       redirect_to new_session_url
     else
       redirect_to users_url
