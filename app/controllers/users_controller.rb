dfd3d888d34f764967df0b30bb26f0a4a27f0650
Tony Won <tonywon89@gmail.com>
2016-03-31 10:53:12 -0700

    Finish user sign up functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,28 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+    render :index
+  end
+
   def new
+    @user = User.new
+    render :new
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to users_url
+    else
+      flash.now[:errors] = "Failed to sign up"
+      render :new
+    end
+
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
