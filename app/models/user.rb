8acd308e75674825ea2ae309a413a0bdf33a0e1d
Tony Won <tonywon89@gmail.com>
2016-03-31 10:36:19 -0700

    Build User model with validtions and methods for session token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,45 @@
 class User < ActiveRecord::Base
+
+  validates :email, :password_digest, :session_token, presence: true
+  validates :email, uniqueness: true
+  validates :password, length: { minimum: 4, allow_nil: true }
+
+  after_initialize :ensure_session_token
+
+  attr_reader :password
+
+  def self.generate_session_token
+    loop do
+      token = SecureRandom::urlsafe_base64(16)
+      return token unless find_by(session_token: token)
+    end
+  end
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+
+    user && user.is_password?(password) ? user :nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password).to_s
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    save!
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+
 end
