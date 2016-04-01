e7ef0e94f9cecaa5cf811f6f0353259d96b64930
Tony Won <tonywon89@gmail.com>
2016-03-31 17:24:10 -0700

    Finish adding musical notes in lyrics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def ugly_lyrics(lyrics)
+    html = ""
+    lines = lyrics.split("\n")
+    lines.each do |line|
+      html += "&#9835;"
+      html += h(line)
+      html += "\n"
+    end
+
+    html.html_safe
+  end
 end
